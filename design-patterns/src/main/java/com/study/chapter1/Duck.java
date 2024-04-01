d5d42ec22ed5d82cfc917adac47020086d682108
YKJ <rudwns7552@gmail.com>
2024-04-02 07:57:58 +0900

    feat: SimUDuck First Version

---
+++ design-patterns/src/main/java/com/study/chapter1/Duck.java
@@ -0,0 +1,16 @@
+package com.study.chapter1;
+
+public abstract class Duck {
+  public void quack(){
+    System.out.println("Duck.quack");
+  }
+
+  public void swim(){
+    System.out.println("Duck.swim");
+  }
+
+  public void display(){
+    System.out.println("Duck.display");
+  }
+  //기타 오리 메서드
+}
---
+++ design-patterns/src/main/java/com/study/chapter1/MallardDuck.java
@@ -0,0 +1,9 @@
+package com.study.chapter1;
+
+public class MallardDuck extends Duck {
+
+  @Override
+  public void display() {
+    System.out.println("MallardDuck.display");
+  }
+}
