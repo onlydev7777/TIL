cfa068a125a4bd06a947f2bb05e5739c65d9b3ca
onlydev7777 <onlydev7777@gmail.com>
2024-04-02 08:29:42 +0900

    feat: SimUDuck Second Version - 인터페이스 적용

--- design-patterns/src/main/java/com/study/chapter1/RedheadDuck.java
+++ design-patterns/src/main/java/com/study/chapter1/RedheadDuck.java
@@ -1,9 +1,19 @@
 package com.study.chapter1;
 
-public class RedheadDuck extends Duck {
+public class RedheadDuck extends Duck implements Flyable, Quackable {
 
   @Override
   public void display() {
     System.out.println("RedheadDuck.display");
+  }
+
+  @Override
+  public void fly() {
+    System.out.println("RedheadDuck.fly");
+  }
+
+  @Override
+  public void quack() {
+    System.out.println("RedheadDuck.quack");
   }
 }
