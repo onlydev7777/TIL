1e5681403d406edcf12f6adfe797c007b89371c7
onlydev7777 <onlydev7777@gmail.com>
2024-04-12 16:28:44 +0900

    feat: 1) toby-springboot   - build.gradle     > jetty 의존성 제거
      - MyConditionOnClass
        > @ConditionalMyOnClass에 붙은 class 명으로 Class Loader에 Load 된 클래스 유무 파악해서 Bean 등록 여부 결정

---
+++ toby-springboot/src/main/java/com/example/config/ConditionalMyOnClass.java
@@ -0,0 +1,15 @@
+package com.example.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.Conditional;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Conditional(MyConditionOnClass.class)
+public @interface ConditionalMyOnClass {
+
+  String value();
+}
--- toby-springboot/src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
+++ toby-springboot/src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
@@ -1,17 +1,16 @@
 package com.example.config.autoconfig;
 
+import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
-import com.example.config.autoconfig.JettyWebServerConfig.JettyCondition;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
-@Conditional(JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
 
   @Bean("jettyWebServerFactory")
