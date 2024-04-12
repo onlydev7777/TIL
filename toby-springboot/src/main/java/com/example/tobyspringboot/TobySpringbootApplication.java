5791a08952276349ffaef6fc62eeb8378279b0af
onlydev7777 <onlydev7777@gmail.com>
2024-04-12 14:50:53 +0900

    feat: 1) toby-springboot   - MyAutoConfigImportSelector     > selectImports 메서드 재정의       : 패키지를 포함한 class 경로 반환해주면 @Import에서 처리 됨       : ImportCandidates.load 메서드를 사용         > META-INF/spring/[패키지포함 어노테이션경로].imports 파일을 읽어서 Iterate<String>으로 return

--- toby-springboot/src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
+++ toby-springboot/src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
@@ -1,27 +1,10 @@
 package com.example.tobyspringboot;
 
+import com.example.config.MySpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-//@SpringBootApplication
-@Configuration
-@ComponentScan
+@MySpringBootApplication
 public class TobySpringbootApplication {
-
-  @Bean
-  public ServletWebServerFactory servletWebServerFactory() {
-    return new TomcatServletWebServerFactory();
-  }
-
-  @Bean
-  public DispatcherServlet dispatcherServlet() {
-    return new DispatcherServlet();
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(TobySpringbootApplication.class, args);
