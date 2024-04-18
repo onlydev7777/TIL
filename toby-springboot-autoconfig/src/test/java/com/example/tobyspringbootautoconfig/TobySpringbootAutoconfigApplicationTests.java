fdc9498f30e31d4def8253d041f61f29dee467bc
onlydev7777 <onlydev7777@gmail.com>
2024-04-18 17:08:51 +0900

    feat: 1) toby-springboot-autoconfig    - 스프링부트 autoconfiguration이 어떻게 동작하는지 분석    - 스프링부트 자세히보기.md 파일 작성

---
+++ toby-springboot-autoconfig/src/test/java/com/example/tobyspringbootautoconfig/TobySpringbootAutoconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobyspringbootautoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringbootAutoconfigApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
