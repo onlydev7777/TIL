db19405f34bc43be1e01397969d16bf9a2442999
onlydev7777 <onlydev7777@gmail.com>
2024-06-05 17:40:47 +0900

    feat: 1) 프로토타입 패턴 코드 작성

---
+++ inflearn-design-patterns/src/main/java/_05_prototype/_03_java/Student.java
@@ -0,0 +1,17 @@
+package _05_prototype._03_java;
+
+public class Student {
+
+  String name;
+
+  public Student(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+        "name='" + name + '\'' +
+        '}';
+  }
+}
