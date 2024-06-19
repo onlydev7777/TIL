44914997ef581194c4db22e85bdbc8a550398630
onlydev7777 <onlydev7777@gmail.com>
2024-06-19 17:17:13 +0900

    feat: 1) 중재자 패턴 코드 작성

---
+++ inflearn-design-patterns/src/main/java/_03_behavioral_patterns/_17_mediator/_02_after/Restaurant.java
@@ -0,0 +1,10 @@
+package _03_behavioral_patterns._17_mediator._02_after;
+
+import java.time.LocalDateTime;
+
+public class Restaurant {
+
+  public void dinner(Integer id, LocalDateTime dateTime) {
+
+  }
+}
